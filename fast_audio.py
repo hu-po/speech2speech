f603a2849bc6395586e2f216d2555a20cf8e98c0
HugoCMU <hugo.p.cmu@gmail.com>
2023-03-10 10:28:30 -0600

    fucking up my audio drivers

--- fast_audio.py
+++ fast_audio.py
@@ -1,38 +1,35 @@
-import gradio as gr
+import time
 import speech_recognition as sr
-import threading
 
+
+# this is called from the background thread
 def callback(recognizer, audio):
+    # received audio data, now we'll recognize it using Google Speech Recognition
     try:
-        # text = recognizer.recognize_google(audio)
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
         print("Audio length: {} seconds".format(len(audio.get_raw_data()) / 16000))
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-def background_listen(m, callback, stopper):
-    r = sr.Recognizer()
-    with m as source:
-        r.adjust_for_ambient_noise(source)
-    with sr.Microphone() as source:
-        r.energy_threshold = 4000
-        while not stopper():
-            audio = r.listen(source, phrase_time_limit=5)
-            callback(r, audio)
 
-stopper = gr.FlaggingCallback()
+r = sr.Recognizer()
+m = sr.Microphone()
+with m as source:
+    r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
 
-audio_input = gr.Microphone(label="Record Audio")
-text_output = gr.outputs.Textbox()
+# start listening in the background (note that we don't have to do this inside a `with` statement)
+stop_listening = r.listen_in_background(m, callback)
+# `stop_listening` is now a function that, when called, stops background listening
 
-iface = gr.Interface(fn=None, inputs=audio_input, outputs=text_output, 
-                     title='Real-Time Audio Transcription', live=True)
+# do some unrelated computations for 5 seconds
+for _ in range(50): time.sleep(0.1)  # we're still listening even though the main thread is doing other things
 
-bg_thread = threading.Thread(target=background_listen, args=(sr.Microphone(), callback, stopper))
-bg_thread.start()
+# calling this function requests that the background listener stop listening
+stop_listening(wait_for_stop=False)
 
-iface.launch()
-
-stopper.set()  # stop the background thread when the interface is closed
-bg_thread.join()
+# do some more unrelated things
+while True: time.sleep(0.1)  # we're not listening anymore, even though the background thread might still be running for a second or two while cleaning up and stopping
