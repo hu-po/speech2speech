866792a8994a62b49fd34a284aa9573741203032
HugoCMU <hugo.p.cmu@gmail.com>
2023-03-17 11:36:05 -0500

    pycache

--- release_hf.sh
+++ release_hf.sh
@@ -1,6 +1,6 @@
 export RELEASE_VERSION=0.2
 cp app.py ../hf_speech2speech/app.py
-cp -rf src/* ../hf_speech2speech/src/*
+cp -rf src/ ../hf_speech2speech/src/
 cp voices.yaml ../hf_speech2speech/voices.yaml
 cp requirements.txt ../hf_speech2speech/requirements.txt
 cp packages.txt ../hf_speech2speech/packages.txt
